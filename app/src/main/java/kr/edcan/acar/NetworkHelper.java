a42745b2bd4c079bbe9f88c87c9756bc6e75c03f
Junseok Oh <kotohana5706@edcan.kr>
2016-09-03 04:56:58 +0900

    Update NetworkHelper

---
+++ app/src/main/java/kr/edcan/acar/NetworkHelper.java
@@ -0,0 +1,35 @@
+package kr.edcan.acar;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by JunseokOh on 2016. 9. 3..
+ */
+public class NetworkHelper {
+    private Context context;
+
+    public NetworkHelper(Context context) {
+        this.context = context;
+    }
+
+    public static Retrofit retrofit;
+
+    public static NetworkInterface getNetworkInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://kafuuchino.one:8000/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(NetworkInterface.class);
+    }
+
+    public static boolean returnNetworkState(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
