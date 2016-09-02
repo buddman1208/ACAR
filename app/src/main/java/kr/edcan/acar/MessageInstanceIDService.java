c2d3deb479b109e90df1e7eed8abece84b80d19f
Junseok Oh <kotohana5706@edcan.kr>
2016-09-03 04:26:07 +0900

    Initialize Firebase

---
+++ app/src/main/java/kr/edcan/acar/MessageInstanceIDService.java
@@ -0,0 +1,11 @@
+package kr.edcan.acar;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by JunseokOh on 2016. 9. 3..
+ */
+public class MessageInstanceIDService extends FirebaseInstanceIdService{
+
+}
---
+++ app/src/main/java/kr/edcan/acar/MessagingService.java
@@ -0,0 +1,10 @@
+package kr.edcan.acar;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by JunseokOh on 2016. 9. 3..
+ */
+public class MessagingService extends FirebaseMessagingService{
+
+}
