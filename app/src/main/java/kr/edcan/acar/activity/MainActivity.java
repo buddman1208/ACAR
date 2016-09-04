8980c62b8e3f856f8381bfb2c22f899adceaba88
Junseok Oh <kotohana5706@edcan.kr>
2016-09-04 10:40:23 +0900

    [MainActivity] update ui with sensor status

--- app/src/main/java/kr/edcan/acar/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/acar/activity/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         dialog = builder.build();
 //        startService(new Intent(getApplicationContext(), MessageInstanceIDService.class));
 //        startService(new Intent(getApplicationContext(), MessagingService.class));
-        updateUI();
         FirebaseInstanceId.getInstance().getToken();
         bt = new BluetoothSPP(this);
 
@@ -76,17 +75,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bt.setAutoConnectionListener(new BluetoothSPP.AutoConnectionListener() {
-            public void onNewConnection(String name, String address) {
-                Log.e("asdf", name);
-            }
-
-            public void onAutoConnectionStarted() {
-            }
-        });
         bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
             public void onDataReceived(byte[] data, String message) {
-                Log.e("asdf", message + " asdf");
+                updateUI(message);
             }
         });
 
@@ -111,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setup() {
-        bt.autoConnect("songjunfuck");
+        bt.autoConnect("haejukwang");
     }
 
 
@@ -150,56 +141,51 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void updateUI() {
+    void updateUI(String originStr) {
         Random random = new Random();
-        final String originStr = random.nextInt(2) + "," + random.nextInt(4) + "," + random.nextInt(2) + "," + random.nextInt(100);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                String[] status = originStr.split(",");
+//        final String originStr = random.nextInt(2) + "," + random.nextInt(4) + "," + random.nextInt(2) + "," + random.nextInt(100);
+        String[] status = originStr.split(",");
         /*뒷자리애0 1,앞자리 사람종류 1어른 2어린이 3애기,문에 누구있음1 없음0,온도*/
-                Resources res = getResources();
-                int back = Integer.parseInt(status[0]);
-                int front = Integer.parseInt(status[1]);
-                int door = Integer.parseInt(status[2]);
-                int temp = Integer.parseInt(status[3]);
+        Resources res = getResources();
+        int back = Integer.parseInt(status[0]);
+        int front = Integer.parseInt(status[1]);
+        int door = Integer.parseInt(status[2]);
+        int temp = Integer.parseInt(status[3]);
+        int engine = Integer.parseInt(status[4]);
 
-                binding.mainCarTemp.setText(temp + "");
-                binding.mainFrontIcon.setImageDrawable(res.getDrawable((front == 0) ? R.drawable.acc_status_seat_off : R.drawable.acc_status_seat_on));
-                binding.mainRearIcon.setImageDrawable(res.getDrawable((back == 0) ? R.drawable.acc_status_seat_off : R.drawable.acc_status_seat_on));
-                binding.mainFrontBackground.setBackgroundResource((front == 0) ? R.drawable.round_10dp_not_colored : R.drawable.round_10dp_colored);
-                binding.mainRearBackground.setBackgroundResource((back == 0) ? R.drawable.round_10dp_not_colored : R.drawable.round_10dp_colored);
+        binding.mainCarTemp.setText(temp + "");
+        binding.mainFrontIcon.setImageDrawable(res.getDrawable((front == 0) ? R.drawable.acc_status_seat_off : R.drawable.acc_status_seat_on));
+        binding.mainRearIcon.setImageDrawable(res.getDrawable((back == 0) ? R.drawable.acc_status_seat_off : R.drawable.acc_status_seat_on));
+        binding.mainFrontBackground.setBackgroundResource((front == 0) ? R.drawable.round_10dp_not_colored : R.drawable.round_10dp_colored);
+        binding.mainRearBackground.setBackgroundResource((back == 0) ? R.drawable.round_10dp_not_colored : R.drawable.round_10dp_colored);
+        binding.mainEngineText.setText((engine == 0) ? "꺼짐" : "양호");
+        binding.mainRearrText.setTextColor(res.getColor((back == 0) ? R.color.main_not_selected_color : R.color.main_selected_color));
+        binding.mainFrontText.setTextColor(res.getColor(!(front == 0) ? R.color.main_selected_color : R.color.main_not_selected_color));
+        binding.mainFrontSubText.setTextColor(res.getColor(!(front == 0) ? R.color.sub_selected_color : R.color.sub_not_selected_color));
+        binding.mainRearSubText.setTextColor(res.getColor((back == 0) ? R.color.sub_not_selected_color : R.color.sub_selected_color));
 
-                binding.mainRearrText.setTextColor(res.getColor((back == 0) ? R.color.main_not_selected_color : R.color.main_selected_color));
-                binding.mainFrontText.setTextColor(res.getColor(!(front == 0) ? R.color.main_selected_color : R.color.main_not_selected_color));
-                binding.mainFrontSubText.setTextColor(res.getColor(!(front == 0) ? R.color.sub_selected_color : R.color.sub_not_selected_color));
-                binding.mainRearSubText.setTextColor(res.getColor((back == 0) ? R.color.sub_not_selected_color : R.color.sub_selected_color));
-
-                String frontResult;
-                switch (front) {
-                    case 0:
-                        frontResult = "없음";
-                        break;
-                    case 1:
-                        frontResult = "어른";
-                        break;
-                    case 2:
-                        frontResult = "어린이";
-                        break;
-                    case 3:
-                        frontResult = "아기";
-                        break;
-                    default:
-                        frontResult = "없음";
-                }
-                binding.mainRearrText.setText((back == 0) ? "없음" : "탑승함");
-                binding.mainFrontText.setText(frontResult);
-                if (door == 0)
-                    dialog.show();
-                else dialog.dismiss();
-                updateUI();
-            }
-        }, 500);
+        String frontResult;
+        switch (front) {
+            case 0:
+                frontResult = "없음";
+                break;
+            case 1:
+                frontResult = "어른";
+                break;
+            case 2:
+                frontResult = "어린이";
+                break;
+            case 3:
+                frontResult = "아기";
+                break;
+            default:
+                frontResult = "없음";
+        }
+        binding.mainRearrText.setText((back == 0) ? "없음" : "탑승함");
+        binding.mainFrontText.setText(frontResult);
+        if (door == 0)
+            dialog.show();
+        else dialog.dismiss();
     }
 
 }
